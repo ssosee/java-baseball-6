4018adda4104fedb0f91cf99eec2d909ed9cdc08
howisitgoing <howisitgoing@kakao.com>
2023-11-18 01:34:37 +0900

    docs(입력): 게임 재시작 확인

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -12,11 +12,11 @@ public class InputView {
     public static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
     public static final String INPUT_MESSAGE = "숫자를 입력해주세요 :";
 
-    public void printGameStartMessage() {
+    public void printGameStart() {
         System.out.println(GAME_START_MESSAGE);
     }
 
-    public List<Integer> printInputNumberMessage() {
+    public List<Integer> printInputNumber() {
         System.out.print(INPUT_MESSAGE);
         String userNumber = Console.readLine();
 
@@ -39,5 +39,14 @@ public class InputView {
 
     private boolean isNumeric(String userNumber) {
         return Pattern.matches(USER_NUMBER_REX, userNumber);
+    }
+
+    public int printNewGame() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String response = Console.readLine();
+
+        validationUserNumber(response);
+
+        return Integer.parseInt(response);
     }
 }
